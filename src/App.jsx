b7d838848c97a1312ec5c2a837047394f839675a
Enzo Martolini <enzo.martolini@mailo.com>
2024-12-10 19:37:25 +0100

    amelioration de l'intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import './styles/body.css';
+import './styles/transition.css';
 import './styles/contact.css';
 import './styles/profile.css';
 import './styles/header.css';
 import './styles/mediaQueries.css';
 import './styles/project.css';
 
-import { useState } from 'react'
-import {Header} from './components/header.jsx'
-import {Project} from './components/project.jsx'
-import { Accueil } from './components/accueil.jsx'
-import { Profile } from './components/profile.jsx'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { Header } from './components/header.jsx';
+import { Project } from './components/project.jsx';
+import { Accueil } from './components/accueil.jsx';
+import { Profile } from './components/profile.jsx';
 import { Contact } from './components/contact.jsx';
 
 function App() {
 
-  let arrayProjects = [
+  const arrayProjects = [
     {
       name: "Portfolio Personnel",
       description: "Un site web personnel qui présente mes compétences en développement web, avec une galerie de projets et mes contacts.",
@@ -30,7 +30,7 @@ function App() {
     },
     {
       name: "Site des calanques de Marseille",
-      description: "Un site de reservation de bungalows pour une periode dans les calanques, tout en trouvant des informations sur la destination et des setniers à parcourir.",
+      description: "Un site de reservation de bungalows pour une période dans les calanques, tout en trouvant des informations sur la destination et des sentiers à parcourir.",
       link: "https://github.com/le-plat-jonathan/parc_national",
       language: "Javascript, PHP et SQL"
     },
@@ -40,63 +40,44 @@ function App() {
       link: "https://github.com/Enzo-Martolini/padel_site_e-commerce",
       language: "Javascript, PHP et SQL"
     }
-  ]
+  ];
 
-const [isLoadingIntro, setIsLoadingIntro] = useState(true)
-const [isLoadingMain, setIsLoadingMain] = useState(false)
+  const [currentSection, setCurrentSection] = useState('intro');
+  const [isLoadingIntro, setIsLoadingIntro] = useState(true);
+  const [isLoadingMain, setIsLoadingMain] = useState(false);
 
-const [currentHash, setCurrentHash] = useState(location.hash);
+  useEffect(() => {
+    const timerIntro = setTimeout(() => {
+      setIsLoadingIntro(false);
+    }, 1500);
 
-useEffect(()=> {
-  let handleChangeHash = () => {
-    setCurrentHash(location.hash)
-    console.log(location.hash)
-  }
-  window.addEventListener('hashchange', handleChangeHash)
+    const timerMain = setTimeout(() => {
+      setIsLoadingMain(true);
+      setCurrentSection('main'); // Change la section courante après le délai
+    }, 2500);
 
-  return () => {
-    window.removeEventListener("hashchange", handleChangeHash);
-  };
-
-},[])
-
-useEffect(()=> {
-  const timerIntro = setTimeout(() => {
-    setIsLoadingIntro(false)
-  }, 1500)
-
-  const timerMain = setTimeout(() => {
-    setIsLoadingMain(true)
-  }, 2000)
-
-  const timerDisplay = setTimeout(() => {
-    console.log("check 2s")
-    console.log(document.querySelector('.intro'))
-    document.querySelector('.intro').style.display = "none"
-  }, 2000)
-
-  return () => {clearTimeout(timerIntro); clearTimeout(timerMain); clearTimeout(timerDisplay)  }
-}, [])
+    return () => { clearTimeout(timerIntro); clearTimeout(timerMain); };
+  }, []);
 
   return (
     <>
+      <div className={`intro ${!isLoadingIntro ? 'hidden' : ''}`}>
+        <p className="title">Bienvenue sur mon <b>Portfolio</b></p>
+      </div>
 
-          <div style={isLoadingIntro ? {opacity : 1} : {opacity : 0}} className='intro'>
-            <p className='title'>Bienvenue sur mon <b>Portfolio</b></p>
-          </div>
-          <div style={isLoadingMain ? {opacity : 1} : {opacity : 0, display : "none"}} className='main'>
-          <Header />
-          {currentHash === "#top" || currentHash === "" ? <Accueil opacity={isLoadingMain ? 1 : 0} /> : "" }
-          {currentHash === "#linkProfile" ? <Profile /> : "" }
-          {currentHash === "#linkProject" ? <Project projects={arrayProjects} /> : "" }
-          {currentHash === "#linkContact" ? <Contact /> : "" }
-          
-          
-        </div>
-
+      <div className={`main ${isLoadingMain ? 'visible' : ''}`}>
+        <Header />
+        {currentSection === 'main' && (
+          <>
+            <Accueil />
+            <Profile />
+            <Project projects={arrayProjects} />
+            <Contact />
+          </>
+        )}
+      </div>
     </>
   );
 }
 
-
-export default App
+export default App;
